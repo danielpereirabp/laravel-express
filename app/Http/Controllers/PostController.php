e57c5966b03a48c95788c58b21f081fade30d802
Daniel Pereira <danielpereirabp@gmail.com>
2015-07-08 11:40:26 -0300

    first commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        $posts = [
+            [
+                'title' => 'Primeiro Post',
+                'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, vero, obcaecati, aut, error quam sapiente nemo saepe quibusdam sit excepturi nam quia corporis eligendi eos magni recusandae laborum minus inventore?'
+            ],
+            [
+                'title' => 'Segundo Post',
+                'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, vero, obcaecati, aut, error quam sapiente nemo saepe quibusdam sit excepturi nam quia corporis eligendi eos magni recusandae laborum minus inventore?'
+            ]
+        ];
+
+        return view('post.index', compact('posts'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        $post = [
+            'title' => 'Primeiro Post',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, vero, obcaecati, aut, error quam sapiente nemo saepe quibusdam sit excepturi nam quia corporis eligendi eos magni recusandae laborum minus inventore?'
+        ];
+
+        return view('post.show', compact('post'));
+    }
+}
---
+++ resources/views/post/index.blade.php
@@ -0,0 +1,48 @@
+@extends('template')
+
+@section('content')
+
+	<div class="row">
+
+        <!-- Blog Post Content Column -->
+        <div class="col-lg-8">
+
+            @foreach($posts as $post)
+                <!-- Blog Post -->
+
+                <!-- Title -->
+                <h1>{{ $post['title'] }}</h1>
+
+                <hr>
+
+                <!-- Post Content -->
+                <p><a href="#">{{ $post['body'] }}</a></p>
+
+                <hr>
+            @endforeach
+            
+        </div>
+
+        <!-- Blog Sidebar Widgets Column -->
+        <div class="col-md-4">
+
+            <!-- Blog Search Well -->
+            <div class="well">
+                <h4>Busca</h4>
+                <div class="input-group">
+                    <input type="text" class="form-control">
+                    <span class="input-group-btn">
+                        <button class="btn btn-default" type="button">
+                            <span class="glyphicon glyphicon-search"></span>
+                    </button>
+                    </span>
+                </div>
+                <!-- /.input-group -->
+            </div>
+
+        </div>
+
+    </div>
+    <!-- /.row -->
+
+@stop
